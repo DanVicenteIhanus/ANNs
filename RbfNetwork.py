e7112bcb5738828fb6af4dcbabc4b904db0ba21a
DanVicenteIhanus <dan.vicente.ihanus@gmail.com>
2024-03-02 10:36:52 +0100

    refactor

--- RbfNetwork.py
+++ RbfNetwork.py
@@ -1,27 +1,13 @@
 import numpy as np, random
 from numpy.linalg import lstsq
-from DD2437_Lab2_script import *
-
-
 
 # ======================= #
 # == Helper functions  == #
 # ======================= #
 
-def f_1(x, noise = 0) -> np.ndarray: 
-    return np.sin(2*x) if noise == 0 else np.sin(2*x) + np.random.normal(loc=0, scale=noise, size=np.shape(x))
+def phi(x, mu, sigma) -> np.ndarray:
+    return np.exp(-(x - mu)**2/(2*sigma**2))
 
-def f_2(x, noise = 0) -> np.ndarray: 
-    f_arr = np.zeros_like(x)
-    for i, d in enumerate(x):
-        f_arr[i] = 1 if np.sin(d) >= 0 else -1
-    return f_arr if noise == 0 else f_arr + np.random.normal(loc=0, scale=noise, size=np.shape(x))
-
-def gen_func_data(f, start, stop, step_length, var = 0) -> np.ndarray:
-    '''Generate training/test-data given a function f'''
-    training_interval = np.arange(start = start, stop = stop, step= step_length)
-    test_interval = np.arange(start = start + 0.05, stop = stop, step = step_length)
-    return training_interval, f(training_interval, var), test_interval, f(test_interval, var)
 
 # ======================= #
 # == RBF Network class == #
@@ -104,37 +90,3 @@ class RbfNetwork:
         self.compute_phi_matrix(X)
         self.w, _, _, _ = lstsq(self.phi_matrix, y, rcond=None)  # Corrected to unpack the tuple
         self.mses.append(np.mean((self.predict(X) - y)**2))
-
-
-# ================= #
-# == Simulations == #
-# ================= #
-def plot_functions(ax) -> None:
-    ax.plot(x_2_test, f_1_test_pred_online, c='b', lw=1.3, ls = 'dashed', label='Predictions [on-line]')
-    ax.plot(x_2_test, f_1_test_pred_batch, c='#bf9000', lw=1.3, ls = 'dashed', label='Predictions [batch lsq]')
-    ax.plot(x_2_test, f_1_test, c='r', lw=1.5, alpha=0.7, label='exact')
-    ax.legend(loc='upper right')
-    ax.set_xlim([0,2*np.pi])
-    print(f'MSE with online [epochs = {rbf_network.epochs}] = {round(rbf_network.mses[-1], 4)}')
-    print(f'MSE with batch = {round(rbf_network_batch.mses[-1], 4)}')
-
-if __name__ == '__main__':
-    start = 0.0
-    stop = 2*np.pi
-    step_length = 0.1
-    dim_basis = 5
-    var = 0.1
-    epochs = 100
-    
-    x_1_training, f_1_training, x_1_test, f_1_test = gen_func_data(f_1, start, stop, step_length, var)
-    x_2_training, f_2_training, x_2_test, f_2_test = gen_func_data(f_2, start, stop, step_length, var)
-    rbf_network = RbfNetwork(f_1_training.shape[0], dim_basis, phi, 0.1, 1.0, epochs)
-    rbf_network_batch = RbfNetwork(f_1_training.shape[0], dim_basis, phi, 0.1, 1.0, epochs)
-
-    rbf_network.fit_online(x_1_training, f_1_training)
-    f_1_test_pred_online = rbf_network.predict(x_1_test)
-
-    rbf_network_batch.fit_lsq(x_1_training, f_1_training)
-    f_1_test_pred_batch = rbf_network_batch.predict(x_1_test)
-    fig, ax = plt.subplots(figsize=(5, 4), dpi=300)
-    plot_functions(ax)
